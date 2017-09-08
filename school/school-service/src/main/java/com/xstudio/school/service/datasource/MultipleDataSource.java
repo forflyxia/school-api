b2ecd7ebb6f9a1fc4a965e66300204b97b57d7c0
vxhb夏红波 <hbxia@ctrip.com>
2017-09-08 17:18:23 +0800

    doc html.

--- school/school-service/src/main/java/com/xstudio/school/service/datasource/MultipleDataSource.java
+++ school/school-service/src/main/java/com/xstudio/school/service/datasource/MultipleDataSource.java
@@ -1,7 +1,12 @@
 package com.xstudio.school.service.datasource;
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+@Cacheable
 public class MultipleDataSource extends AbstractRoutingDataSource {
 
     private static final ThreadLocal<String> dataSourceKey = new InheritableThreadLocal<String>();
@@ -9,6 +14,9 @@ public class MultipleDataSource extends AbstractRoutingDataSource {
     public static void setDataSourceKey(String dataSource) {
         dataSource = dataSource.toLowerCase();
         dataSourceKey.set(dataSource);
+        java.util.Hashtable
+        ConcurrentHashMap
+
     }
 
     @Override
